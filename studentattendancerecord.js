452762906a801932d82a466a6f51d7160653f2f1
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-29 05:16:51 -0400

    solutions

---
+++ studentattendancerecord.js
@@ -0,0 +1,48 @@
+//Objective is to see whether a student's attendance record is clean.
+//It is clean if it has no more than 2 absences (A) as well as no more than 
+//2 lates (L) in a row.
+
+let s = "PPALLP"
+
+
+//O(n) solution that does a two-pass on the string. Once to check the 
+//absence count and again to check for simultaneous lates.
+
+let absentCount = 0
+
+//Simple pass to check for absences
+for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) == 'A') {
+        absentCount++
+    }
+}
+    
+if (absentCount > 1) {
+    return false
+}
+    
+let start = 0
+let end = 3
+    
+//Sliding window to check for lates
+while (end <= s.length) {
+    let substring = s.substring(start, end)
+    let map = {}
+        
+    for (let i = 0; i < substring.length; i++) {
+        if (map[substring.charAt(i)] == undefined) {
+            map[substring.charAt(i)] = 1
+        } else {
+            map[substring.charAt(i)]++
+        }
+    }
+        
+    if (map['L'] > 2) {
+        return false
+    }
+        
+    start++
+    end++
+}
+    
+return true
